f519d3b5770a7aaadfef9ca638b5f99002f333c6
Xanderpp <alexanderpatrick.pijan@gmail.com>
2023-02-06 07:49:03 +0800

    update v2.3

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ function typed_js(str)  {
   new Typed('#typed', {
     strings: typed_strings,
     // loop: true,
-    typeSpeed: 100,
+    typeSpeed: 25,
     backSpeed: 50,
     backDelay: 2000
   });
